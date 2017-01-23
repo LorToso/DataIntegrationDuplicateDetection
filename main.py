d80227f28c4f4f94f51a9b84c9fd969f2d4439c8
Lorenzo Toso <LorToso@gmail.com>
2017-01-23 16:24:26 +0100

    importing library

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import Levenshtein as lv
 import csv
 with open("inputDB.csv") as csvfile:
     filereader = csv.reader(csvfile)
     list = []
     for row in filereader:
         list.append(row)
-    for row in list:
-        print(row)
-
+    #for row in list:
+        #print(row)
+print(lv.distance('abc', 'abd'))
 
 

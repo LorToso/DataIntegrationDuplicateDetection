075d14f59caf3b3e72e9f9b38aa295872b7af8d7
Lorenzo Toso <LorToso@gmail.com>
2017-01-23 16:34:07 +0100

    Now comparing files using edit distance

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import Levenshtein as lv
 import csv
+
 with open("inputDB.csv") as csvfile:
     filereader = csv.reader(csvfile)
     list = []
     for row in filereader:
         list.append(row)
-    #for row in list:
-        #print(row)
-print(lv.distance('abc', 'abd'))
 
-
+    res = []
+    i = 0
+    for cand1 in range(1, len(list) - 1):
+        for cand2 in range(cand1 + 1, len(list)):
+            res.append(lv.distance(''.join(list[cand1]), ''.join(list[cand2])))
+            i += 1
+            if i % 100 == 0:
+                print(i)
